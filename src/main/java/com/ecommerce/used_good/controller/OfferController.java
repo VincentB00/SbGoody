26b0eb819fd8b281e5dbbc3a4a5b90f2f09a4a98
Vincent Bui <vincentbuiwork@gmail.com>
2022-08-08 19:57:30 -0400

    LTS 1.0.2 Clean some import fix modify user bug that make modify password to be empty

--- src/main/java/com/ecommerce/used_good/controller/OfferController.java
+++ src/main/java/com/ecommerce/used_good/controller/OfferController.java
@@ -3,10 +3,8 @@ package com.ecommerce.used_good.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.access.annotation.Secured;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.Authentication;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
--- src/main/java/com/ecommerce/used_good/controller/ToolController.java
+++ src/main/java/com/ecommerce/used_good/controller/ToolController.java
@@ -2,7 +2,6 @@ package com.ecommerce.used_good.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
